3be9ca8d8727c83d64324533ab6d1c2e3edfa700
akilasandaruwan <toakila7@gmail.com>
2021-05-05 16:07:27 +0530

    common repository initialized

---
+++ src/main/java/com/epic/ims/service/sysuser/SystemUserService.java
@@ -0,0 +1,9 @@
+package com.epic.ims.service.sysuser;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+@Service
+@Scope("prototype")
+public class SystemUserService {
+}
---
+++ src/main/java/com/epic/ims/util/varlist/PageVarList.java
@@ -0,0 +1,8 @@
+package com.epic.ims.util.varlist;
+
+public class PageVarList {
+    public static final String LOGIN_PAGE = "LGIN";
+    public static final String HOME = "HOME";
+    public static final String USER_MGT = "USMT";
+
+}
